f1dadfb13ed64a579cb3a1a55f7ac997e6da2daf
wuxing.zwx <wuxing.zwx@alibaba-inc.com>
2017-11-23 17:59:21 +0800

    release

--- publish.py
+++ publish.py
@@ -22,7 +22,7 @@ else:
         wf.close
 
     tag = "git tag -a "+version+" -m '"+version+"'" 
-    trunk = 'pod trunk push ' + spec
+    trunk = 'pod trunk push ' + spec + ' --allow-warnings'
 
     os.system('git add .')
     os.system('git commit -m release')
