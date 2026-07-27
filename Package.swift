8821ed2358a5b3877409ba6b971c5eec98e446b9
AliOSStemp <gaoguangzhi.ggz@alibaba-inc.com>
2026-07-27 16:37:09 +0800

    VODUpload 2.0.1: update binaryTarget checksum for refreshed xcframework (refresh-callback fix)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [.library(name: "VODUpload", targets: ["VODUpload", "VODUploadDependencies"])],
     dependencies: [.package(url: "https://github.com/AliOSStemp/AliyunOSSiOS.git", from: "2.11.0")],
     targets: [
-        .binaryTarget(name: "VODUpload", url: "https://alivc-demo-cms.alicdn.com/versionProduct/sourceCode/upload/2.0.1/iOS/VODUpload.xcframework.zip", checksum: "01b3bcfa3741ae46164db95f43af035959d331238eefb5ca677ccdacbf400ca6"),
+        .binaryTarget(name: "VODUpload", url: "https://alivc-demo-cms.alicdn.com/versionProduct/sourceCode/upload/2.0.1/iOS/VODUpload.xcframework.zip", checksum: "22db85a453c8ccec3bbfef9a0bcf00f650ed983b270e03b0b034a2b048a1fb7b"),
         .target(
             name: "VODUploadDependencies",
             dependencies: [.product(name: "AliyunOSSiOS", package: "AliyunOSSiOS")],
